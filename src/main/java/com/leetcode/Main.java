b34238a7f791a0bd767439f3cf51458683e0e186
Denis_Mishin@epam.com <denis_mishin@epam.com>
2021-04-08 10:31:11 +0400

    В принципе работает, но есть переполнение при больших числах, надо изменить принцип решения. 2 Add Two Numbers

---
+++ src/main/java/com/leetcode/Main.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node = solution.addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))));
+        System.out.println(node.val);
+        System.out.println(node.next.val);
+        System.out.println(node.next.next.val);
+    }
+}
--- src/main/java/com/leetcode/Solution.java
+++ src/main/java/com/leetcode/Solution.java
@@ -3,28 +3,24 @@ package com.leetcode;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int target = getNum(l1) + getNum(l2);
-        StringBuilder builder = new StringBuilder();
-        builder.append(target);
-        char[] chars = new char[builder.length()];
-        builder.getChars(0, builder.length(), chars, 0);
-
-        ListNode parentNode = new ListNode();
-
-        for (int i = 0; i < chars.length; i++){
-            parentNode.next = new ListNode();
-            parentNode.val = Integer.parseInt(String.valueOf(chars[i]));
-            parentNode = parentNode.next;
+        ListNode myNode = new ListNode();
+        ListNode finalNode = myNode;
+        while(true){
+            myNode.val = target % 10;
+            target /= 10;
+            if (target == 0) break;
+            myNode.next = new ListNode();
+            myNode = myNode.next;
         }
-        return parentNode;
+        return finalNode;
     }
 
     public int getNum(ListNode node){
-        ListNode myNode = node;
         StringBuilder builder = new StringBuilder();
         while (true){
-            builder.append(myNode.val);
-            if (myNode.next == null) break;
-            myNode =  myNode.next;
+            builder.append(node.val);
+            if (node.next == null) break;
+            node =  node.next;
         }
         builder.reverse();
         return Integer.parseInt(builder.toString());
