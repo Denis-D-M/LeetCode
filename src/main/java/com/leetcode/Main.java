4b7a124eb9000dd9b9d136d97489b6889b86a59f
Denis_Mishin@epam.com <denis_mishin@epam.com>
2021-04-08 13:36:12 +0400

    2 Add Two Numbers Runtime: 2 ms, faster than 76.46% of Java online submissions for Add Two Numbers. Memory Usage: 39.3 MB, less than 54.06% of Java online submissions for Add Two Numbers. Решил сам (пусть и за 2 дня), повод для гордости XD

--- src/main/java/com/leetcode/Main.java
+++ src/main/java/com/leetcode/Main.java
@@ -3,8 +3,32 @@ package com.leetcode;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode node = solution.addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))));
-//        ListNode node = solution.addTwoNumbers(new ListNode(0), new ListNode(0));
+        ListNode listNode = new ListNode(9);
+        ListNode listNode1 = new ListNode(9);
+        ListNode listNode2 = new ListNode(9);
+        ListNode listNode3 = new ListNode(9);
+        ListNode listNode4 = new ListNode(9);
+        ListNode listNode5 = new ListNode(9);
+        ListNode listNode6 = new ListNode(9);
+        ListNode listNode7 = new ListNode(9);
+        ListNode listNode8 = new ListNode(9);
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+        listNode6.next = listNode7;
+        listNode7.next = listNode8;
+        ListNode node0 = new ListNode(9);
+        ListNode node1 = new ListNode(9);
+        ListNode node2 = new ListNode(9);
+        ListNode node3 = new ListNode(9);
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode node = solution.addTwoNumbers(listNode, node0);
         System.out.println(node.val);
         System.out.println(node.next.val);
         System.out.println(node.next.next.val);
--- src/main/java/com/leetcode/Solution.java
+++ src/main/java/com/leetcode/Solution.java
@@ -5,29 +5,25 @@ class Solution {
         ListNode myNode = new ListNode();
         ListNode finalNode = myNode;
         int rest = 0;
-        boolean create = false;
+
         while(true) {
             myNode.val = (l1.val + l2.val + rest) % 10;
-            rest = (l1.val + l2.val) / 10;
+            rest = (l1.val + l2.val + rest) / 10;
             l1.val = 0;
             if (l1.next != null) {
                 l1 = l1.next;
-                create = true;
             }
             l2.val = 0;
             if (l2.next != null) {
                 l2 = l2.next;
-                create = true;
             }
-            if (create) {
-                myNode.next = new ListNode();
-                myNode = myNode.next;
-            }
-            if (l1.next == null && l2.next == null) break;
+            if (l1.val == 0 && l2.val == 0 && l1.next == null && l2.next == null &&  rest == 0) break;
 
+            myNode.next = new ListNode();
+            myNode = myNode.next;
 
         }
-        myNode.val = (l1.val + l2.val + rest) % 10;
+
         return finalNode;
     }
 }
